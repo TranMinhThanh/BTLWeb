974d5cdaf57ce5ea492beb58307a0277ac3eadd9
thanhtm <thanhtm59cntt@gmail.com>
2017-12-31 00:05:30 +0700

    Merge editRequest

--- resources/views/editRequest.blade.php
+++ resources/views/editRequest.blade.php
@@ -145,7 +145,7 @@
         </div>
     </div>
     <script type="text/javascript">
-            document.write("adfsdf");
+        $(document).ready(function () {
             $('#priority').hide();
             $('#deadline').hide();
             $('#team').hide();
@@ -157,37 +157,39 @@
             $('#cancel').hide();
             $('#cancel').click(cancel);
 
+            $( "#relater")
+                .on("keydown", function( event ) {
+                    //document.write("sdfasd");
+                    if ( event.keyCode === 9 &&
+                        $(this).autocomplete( "instance" ).menu.active ) {
+                        event.preventDefault();
+                    }
+                })
+                .autocomplete({
+                    source: function( request, response ) {
+                        $.getJSON( '{{url('search/autocomplete/editRequest/'.$request->id)}}', {
+                            term: extractLast( request.term )
+                        }, response );
+                    },
+                    focus: function() {
+                        // prevent value inserted on focus
+                        return false;
+                    },
+                    select: function( event, ui ) {
+                        var terms = split( this.value );
+                        // remove the current input
+                        terms.pop();
+                        // add the selected item
+                        terms.push( ui.item.value );
+                        // add placeholder to get the comma-and-space at the end
+                        terms.push( "" );
+                        this.value = terms.join( ", " );
+                        return false;
+                    }
+                });
+
+        });
         //sử dụng autocomplete với input có id = key
-        $( "#relater")
-            .on("keydown", function( event ) {
-                //document.write("sdfasd");
-                 if ( event.keyCode === 9 &&
-                     $(this).autocomplete( "instance" ).menu.active ) {
-                      event.preventDefault();
-                }
-            })
-            .autocomplete({
-                source: function( request, response ) {
-                    $.getJSON( '{{url('search/autocomplete/editRequest/'.$request->id)}}', {
-                        term: extractLast( request.term )
-                    }, response );
-                },
-                focus: function() {
-                    // prevent value inserted on focus
-                    return false;
-                },
-                select: function( event, ui ) {
-                    var terms = split( this.value );
-                    // remove the current input
-                    terms.pop();
-                    // add the selected item
-                    terms.push( ui.item.value );
-                    // add placeholder to get the comma-and-space at the end
-                    terms.push( "" );
-                    this.value = terms.join( ", " );
-                    return false;
-                }
-            });
         function extractLast( term ) {
             return split( term ).pop();
         }
